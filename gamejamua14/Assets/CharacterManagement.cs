1ea3220b2b67748374fa34ccf4f74f76d4889420
Héctor Barreras Almarcha <hbaelx@gmail.com>
2014-07-11 22:24:46 +0200

    - Player switches through characters when entering a room

--- gamejamua14/Assets/CharacterManagement.cs
+++ gamejamua14/Assets/CharacterManagement.cs
@@ -3,22 +3,44 @@ using System.Collections;
 
 public class CharacterManagement : MonoBehaviour
 {
+		public GameObject BasicPlayer;
+		public GameObject SpidersPlayer;
+
 		private	bool expectingChange;
+		private GameObject currentPlayer;
 
 		void Start ()
 		{
 				expectingChange = false;
+				currentPlayer = BasicPlayer;
 		}
 
 		void OnTriggerExit (Collider a_collider)
 		{
 				expectingChange = true;
+
 		}
 
 		void OnTriggerStay (Collider a_collider)
 		{
-				switch (a_collider.name) {
+				if (!expectingChange)
+						return;
 
+				currentPlayer.SetActive (false);	
+
+				switch (a_collider.name) {
+				case "TriggerSpiders":
+						SpidersPlayer.SetActive (true);
+						currentPlayer = SpidersPlayer;
+						break;
+
+				case "TriggerBase":
+						BasicPlayer.SetActive (true);
+						currentPlayer = BasicPlayer;
+						break;
 				}
+
+				//End the change
+				expectingChange = false;
 		}
 }
