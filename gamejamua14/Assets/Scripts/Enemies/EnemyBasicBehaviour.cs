3c6b4097f46ff6ec99effb67d01eccf001904db3
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2014-07-11 21:39:31 +0200

    - Basic stuff for EnemyBehaviour

--- gamejamua14/Assets/Scripts/Enemies/EnemyBasicBehaviour.cs
+++ gamejamua14/Assets/Scripts/Enemies/EnemyBasicBehaviour.cs
@@ -1,15 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyBasicBehaviour : MonoBehaviour {
+public class EnemyBasicBehaviour : MonoBehaviour, IKillable, IAttackable {
 
-	// Use this for initialization
-	void Start () {
-	
+	public static GameControl GameCtrl;
+
+	public int MaxHealth;
+	protected float	health;
+
+	public bool busy;
+	public bool Damage;
+	public bool Die;
+	public bool chasingPlayer;
+
+	public int _MaxHealth {
+		get {
+			return MaxHealth;
+		}
+		set {
+			MaxHealth = value;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public float _Health {
+		get {
+			return health;
+		}
+		
+		set {
+			health = value;
+		}
+	}
 	
+	public GameObject _OwnedBy {
+		get;
+		set;
+	}
+
+	public void _TakeDamage (float a_damage, Vector3 a_hitPoint)
+	{
+		Damage = true;
+		// StartCoroutine (TestCoroutine ()); //TODO: replace
+		health -= a_damage;
+		if (health <= 0)
+			_Die ();
+	}
+	
+	public void _Die ()
+	{
+		health = 0;
+		// GameCtrl.ZombiesOnStage--; //TODO: replace 
+		Die = true;
+	}
+
+	void Start ()
+	{
+		//TODO: fill this
+	}
+
+	void FixedUpdate ()
+	{
+		//TODO: fill this
+	}
+
+	void checkPlayerVisibility ()
+	{
+		//Already chasing the player?
+		if (chasingPlayer)
+			return; //Yes, return
+		
+		//Direct vision with player? Go chase that little bitch
+		if (gameObject.Ext_DirectRay (HeadTransform.position, PlayerRefs.Player.transform.position, PlayerRefs.Player.collider)) {
+			StartCoroutine (chasePlayer ());
+		}
+	}
+
+	protected IEnumerator chasePlayer ()
+	{
+
 	}
 }
