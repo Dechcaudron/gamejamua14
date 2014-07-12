a9acc2c04073e7a6517411016476af9e55fccbb6
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2014-07-12 17:54:37 +0200

    - Added menu

---
+++ gamejamua14/Assets/Scripts/MenuControllerB.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuControllerB : MonoBehaviour
+{
+
+		public GameObject[] SelectableGameobjects;
+		public TextMesh AboutTextMesh;
+		public float MovementSpeed;
+		public float EndRotation;
+		public float EndX;
+
+		private int selectedIndex {
+				get {
+						return h_selIn;
+				}
+				set {
+						if (value < 0)
+								h_selIn = SelectableGameobjects.Length - 1;
+						else if (value > SelectableGameobjects.Length - 1)
+								h_selIn = 0;
+						else
+								h_selIn = value;
+				}
+		}
+		private int h_selIn;
+
+		void Start ()
+		{
+				selectedIndex = 0;
+		}
+
+		void Update ()
+		{
+				if (Input.GetKeyDown (KeyCode.UpArrow)) {
+						selectedIndex--;
+				}
+
+				if (Input.GetKeyDown (KeyCode.DownArrow)) {
+						selectedIndex++;
+				}
+
+				if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space)) {
+						switch (selectedIndex) {
+							case 0:
+									Application.LoadLevel ("MainGame");
+									break;
+
+							case 1:
+									//TODO: add about
+									break;
+						}
+				}
+		}
+
+		void FixedUpdate ()
+		{
+				for (int i=0; i<SelectableGameobjects.Length; i++) {
+						Transform b_transf = SelectableGameobjects [i].transform;
+						if (i == selectedIndex) {
+								b_transf.position = new Vector3 (Mathf.Lerp (b_transf.position.x, EndX, MovementSpeed * Time.deltaTime), b_transf.position.y, b_transf.position.z);
+								b_transf.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Lerp (b_transf.rotation.eulerAngles.y, EndRotation, MovementSpeed * Time.deltaTime)));
+						} else {
+								b_transf.position = new Vector3 (Mathf.Lerp (b_transf.position.x, 0, MovementSpeed * Time.deltaTime), b_transf.position.y, b_transf.position.z);
+								b_transf.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Lerp (b_transf.rotation.eulerAngles.y, 0, MovementSpeed * Time.deltaTime)));
+						}
+				}
+		}
+}
---
+++ gamejamua14/Assets/Scripts/MenuMusicBackground.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusicBackground : MonoBehaviour {
+	
+	public AudioSource clip;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (!clip.isPlaying) {
+			clip.Play ();
+		}
+		
+	}
+}
