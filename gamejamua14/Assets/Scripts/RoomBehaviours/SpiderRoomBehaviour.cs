90b8bcd6f937e63f279df4d3446eec6d5e3f3e17
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2014-07-12 11:58:06 +0200

    - Party hard

--- gamejamua14/Assets/Scripts/RoomBehaviours/SpiderRoomBehaviour.cs
+++ gamejamua14/Assets/Scripts/RoomBehaviours/SpiderRoomBehaviour.cs
@@ -35,44 +35,46 @@ public class SpiderRoomBehaviour : NightmareRoomBehaviour
 				int t_toSpawn = Mathf.RoundToInt (EnemyDensity * MadnessPercentage);
 				int t_leftToSpawn = t_toSpawn;
 
-			
-				if (MadnessPercentage < 0.33) {
-						//Spawn little shit
-						while (t_leftToSpawn-- >0) {
-								spawnSpider (SpiderSpawns.GetRandomTransform ());
-						}
+					if (MadnessPercentage < 0.33) {
+							//Spawn little shit
+							while (t_leftToSpawn-- >0) {
+									spawnSpider (SpiderSpawns.GetRandomTransform ());
+							}
 
-				} else if (MadnessPercentage < 0.66) {
-						//Spawn spiderwebs and little shit
-						while (t_leftToSpawn-->t_toSpawn*0.2) {
-								//Little shit
-								spawnSpider (SpiderSpawns.GetRandomTransform ());
-						}
-						while (t_leftToSpawn-->0) {					
-								//Spiderwebs
-								GameObject t_newWeb = GameObject.Instantiate (Spiderweb) as GameObject;
+					} else if (MadnessPercentage < 0.66) {
+							//Spawn spiderwebs and little shit
+							while (t_leftToSpawn-->t_toSpawn*0.2) {
+									//Little shit
+									spawnSpider (SpiderSpawns.GetRandomTransform ());
+							}
+							while (t_leftToSpawn-->0) {					
+									//Spiderwebs
+									GameObject t_newWeb = GameObject.Instantiate (Spiderweb) as GameObject;
 
-								t_newWeb.transform.position = SpiderSpawns.GetRandomTransform ().position;
-						}
-				} else {
-						//Spawn the shitload
-						while (t_leftToSpawn-->t_toSpawn*0.5) {
-								//Little shit
-								spawnSpider (SpiderSpawns.GetRandomTransform ());
-						}
-						while (t_leftToSpawn-->0) {					
-								//Spiderwebs
-								GameObject t_newWeb = GameObject.Instantiate (Spiderweb) as GameObject;
-				
-								t_newWeb.transform.position = SpiderSpawns.GetRandomTransform ().position;
-						}
+									t_newWeb.transform.position = SpiderSpawns.GetRandomTransform ().position;
+							}
+					} else {
+							//Spawn the shitload
+							while (t_leftToSpawn-->t_toSpawn*0.5) {
+									//Little shit
+									spawnSpider (SpiderSpawns.GetRandomTransform ());
+							}
+							while (t_leftToSpawn-->0) {					
+									//Spiderwebs
+									GameObject t_newWeb = GameObject.Instantiate (Spiderweb) as GameObject;
+					
+									t_newWeb.transform.position = SpiderSpawns.GetRandomTransform ().position;
+							}
 				}
+			
 		}
 
 		void spawnSpider (Transform a_spawn)
 		{
-				GameObject t_newSpider = GameObject.Instantiate (Spider, a_spawn.position, Quaternion.identity) as GameObject;
-
-				t_newSpider.GetComponent<SpiderBehaviour> ().SetSpawn (a_spawn);
+				if (LightMobs.Count < MAX_LIGHTMOBS_PER_SCENE) {
+						GameObject t_newSpider = GameObject.Instantiate (Spider, a_spawn.position, Quaternion.identity) as GameObject;
+						LightMobs.Add (t_newSpider);
+						t_newSpider.GetComponent<SpiderBehaviour> ().SetSpawn (a_spawn);
+				}
 		}
 }
