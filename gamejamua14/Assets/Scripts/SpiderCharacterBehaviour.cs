0e14a6f9d6a2549b5b7de0ece701a508f495f0b9
Héctor Barreras Almarcha <hbaelx@gmail.com>
2014-07-12 02:50:40 +0200

    - Sword and other shit moves and shit

--- gamejamua14/Assets/Scripts/SpiderCharacterBehaviour.cs
+++ gamejamua14/Assets/Scripts/SpiderCharacterBehaviour.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class SpiderCharacterBehaviour : BasicCharacterBehaviour
 {
 
+		public Animator SwordAnimator;
+
 		protected override void Start ()
 		{
 				StaticRefs.Refs.SpiderPlayer = gameObject;
 				gameObject.SetActive (false);
+		}
+
+		protected override void ProcessMouseClicks ()
+		{
+				base.ProcessMouseClicks ();
+
+				if (Input.GetMouseButtonDown (0)) {
+						SwordAnimator.SetTrigger (HashIDs.Attack);
+				}
 		}
 }
