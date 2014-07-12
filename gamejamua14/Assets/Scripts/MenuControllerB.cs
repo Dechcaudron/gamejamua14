73f677b484a035ccf2de98900c2faf9627075bb8
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2014-07-12 19:04:41 +0200

    - Added music to menu

--- gamejamua14/Assets/Scripts/MenuControllerB.cs
+++ gamejamua14/Assets/Scripts/MenuControllerB.cs
@@ -4,66 +4,91 @@ using System.Collections;
 public class MenuControllerB : MonoBehaviour
 {
 
-		public GameObject[] SelectableGameobjects;
-		public TextMesh AboutTextMesh;
-		public float MovementSpeed;
-		public float EndRotation;
-		public float EndX;
+	public GameObject[] SelectableGameobjects;
+	public TextMesh AboutTextMesh;
+	public float MovementSpeed;
+	public float EndRotation;
+	public float EndX;
+	public AudioSource clip;
+	[Range(0.1f, 1f)]
+	public float
+		fadeSteps;
+	private bool fadingout = false;
 
-		private int selectedIndex {
-				get {
-						return h_selIn;
-				}
-				set {
-						if (value < 0)
-								h_selIn = SelectableGameobjects.Length - 1;
-						else if (value > SelectableGameobjects.Length - 1)
-								h_selIn = 0;
-						else
-								h_selIn = value;
-				}
+	private int selectedIndex {
+		get {
+			return h_selIn;
 		}
-		private int h_selIn;
+		set {
+			if (value < 0)
+				h_selIn = SelectableGameobjects.Length - 1;
+			else if (value > SelectableGameobjects.Length - 1)
+				h_selIn = 0;
+			else
+				h_selIn = value;
+		}
+	}
 
-		void Start ()
-		{
-				selectedIndex = 0;
+	private int h_selIn;
+
+	void Start ()
+	{
+		selectedIndex = 0;
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			selectedIndex--;
 		}
 
-		void Update ()
-		{
-				if (Input.GetKeyDown (KeyCode.UpArrow)) {
-						selectedIndex--;
-				}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			selectedIndex++;
+		}
 
-				if (Input.GetKeyDown (KeyCode.DownArrow)) {
-						selectedIndex++;
-				}
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space)) {
+			switch (selectedIndex) {
+			case 0:
+				FadeOut ();
+				break;
 
-				if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.Space)) {
-						switch (selectedIndex) {
-							case 0:
-									Application.LoadLevel ("MainGame");
-									break;
-
-							case 1:
+			case 1:
 									//TODO: add about
-									break;
-						}
-				}
+				break;
+			}
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if (!clip.isPlaying) {
+			clip.Play ();
+		}
+				
+		if (fadingout && clip.volume > 0.0) {
+			FadeOut ();
 		}
 
-		void FixedUpdate ()
-		{
-				for (int i=0; i<SelectableGameobjects.Length; i++) {
-						Transform b_transf = SelectableGameobjects [i].transform;
-						if (i == selectedIndex) {
-								b_transf.position = new Vector3 (Mathf.Lerp (b_transf.position.x, EndX, MovementSpeed * Time.deltaTime), b_transf.position.y, b_transf.position.z);
-								b_transf.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Lerp (b_transf.rotation.eulerAngles.y, EndRotation, MovementSpeed * Time.deltaTime)));
-						} else {
-								b_transf.position = new Vector3 (Mathf.Lerp (b_transf.position.x, 0, MovementSpeed * Time.deltaTime), b_transf.position.y, b_transf.position.z);
-								b_transf.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Lerp (b_transf.rotation.eulerAngles.y, 0, MovementSpeed * Time.deltaTime)));
-						}
-				}
+		if(fadingout && clip.volume == 0.0){
+			Application.LoadLevel ("MainGame");
 		}
+
+		for (int i=0; i<SelectableGameobjects.Length; i++) {
+			Transform b_transf = SelectableGameobjects [i].transform;
+			if (i == selectedIndex) {
+				b_transf.position = new Vector3 (Mathf.Lerp (b_transf.position.x, EndX, MovementSpeed * Time.deltaTime), b_transf.position.y, b_transf.position.z);
+				b_transf.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Lerp (b_transf.rotation.eulerAngles.y, EndRotation, MovementSpeed * Time.deltaTime)));
+			} else {
+				b_transf.position = new Vector3 (Mathf.Lerp (b_transf.position.x, 0, MovementSpeed * Time.deltaTime), b_transf.position.y, b_transf.position.z);
+				b_transf.rotation = Quaternion.Euler (new Vector3 (0, Mathf.Lerp (b_transf.rotation.eulerAngles.y, 0, MovementSpeed * Time.deltaTime)));
+			}
+		}
+	}
+
+	public void FadeOut ()
+	{
+		print ("fadding out");
+		fadingout = true;
+		clip.volume -= ((float)fadeSteps) * Time.deltaTime;
+	}
 }
